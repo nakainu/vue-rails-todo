27271bfdc647dabff3a9777cb6a0d9fc8891c33f
nakainu <c011619379@edu.teu.ac.jp>
2019-09-03 02:24:12 +0900

    fixed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,5 +4,4 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
-environment.loaders.get('vue').use[0].options.extractCSS = false
 module.exports = environment
